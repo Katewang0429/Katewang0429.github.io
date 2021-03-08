a38f3d76fb2912bb9a3eb0b8af625cbabeb86053
Kate Wang <64306646+Katewang0429@users.noreply.github.com>
2021-03-09 01:46:06 +0800

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,67 @@
+def calculate(min, max):
+    # 請用你的程式補完這個函式的區塊
+    sum=0
+    n=min
+    while n<=max:
+        sum=sum+n
+        n=n+1
+    print(sum)
+
+calculate(1,3)
+calculate(4,8)
+
+
+#Python字典與列表
+def avg(data):
+    # 請用你的程式補完這個函式的區塊
+    sum=0
+    result=0
+    count=data["count"]
+    for item in data["employees"]:
+        sum=sum + item["salary"]
+
+    result=sum/count
+    print(result)
+
+avg({
+    "count":3,
+    "employees":[
+        {
+            "name":"John",
+            "salary":30000
+        },
+        {
+            "name":"Bob",
+            "salary":60000
+        },
+        {
+            "name":"Jenny",
+            "salary":50000
+        }
+    ]
+})
+
+def maxProduct(nums):
+  product = []
+  for i in range(len(nums)-1):
+      for j in range(i+1, len(nums)):
+          product.append(nums[i]*nums[j])
+  return max(product)
+    
+print(maxProduct([5,20,2,6]))
+print(maxProduct([10,-20,0,3]))
+
+
+def twoSum(nums, target):
+  Sum = []
+  for i in range(len(nums)-1):
+    for j in range(i+1, len(nums)):
+      if nums[i]+nums[j]== target:
+        Sum.append(i)
+        Sum.append(j)
+        break
+  return Sum
+
+result = twoSum([2,11,7,15],9)
+print(result)
+
