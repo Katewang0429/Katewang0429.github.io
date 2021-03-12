4a17d52e994952d486ad84ef8cf0d6aaa2c5322e
Kate Wang <64306646+Katewang0429@users.noreply.github.com>
2021-03-12 18:28:53 +0800

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -1,51 +1,47 @@
 def calculate(min, max):
-    # 請用你的程式補完這個函式的區塊
-    sum=0
-    n=min
-    while n<=max:
-        sum=sum+n
-        n=n+1
+    sum = 0
+    n = min
+    while n <= max:
+        sum = sum + n
+        n = n + 1
     print(sum)
 
-calculate(1,3)
-calculate(4,8)
+calculate(1, 3)# 你的程式要能夠計算 1+2+3，最後印出 6
+calculate(4, 8)# 你的程式要能夠計算 4+5+6+7+8，最後印出 30
 
-
-#Python字典與列表
 def avg(data):
-    # 請用你的程式補完這個函式的區塊
-    sum=0
-    result=0
-    count=data["count"]
+    sum = 0
+    result = 0
+    count = data["count"]   
     for item in data["employees"]:
-        sum=sum + item["salary"]
+      sum = sum + item["salary"]             
+    result = sum / count
 
-    result=sum/count
     print(result)
 
 avg({
-    "count":3,
-    "employees":[
+     "count":3,
+     "employees":[
         {
-            "name":"John",
-            "salary":30000
+         "name":"John",
+         "salary":30000
         },
         {
-            "name":"Bob",
-            "salary":60000
+         "name":"Bob",
+        "salary":60000
         },
         {
-            "name":"Jenny",
-            "salary":50000
-        }
-    ]
-})
+         "name":"Jenny",
+         "salary":50000
+     }
+     ]
+}) # 呼叫 avg 函式
 
 def maxProduct(nums):
   product = []
   for i in range(len(nums)-1):
-      for j in range(i+1, len(nums)):
-          product.append(nums[i]*nums[j])
+    for j in range(i+1, len(nums)):
+        product.append(nums[i]*nums[j])
   return max(product)
     
 print(maxProduct([5,20,2,6]))
@@ -65,3 +61,4 @@ def twoSum(nums, target):
 result = twoSum([2,11,7,15],9)
 print(result)
 
+    
