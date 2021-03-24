977f9e1dce4af1b81efb3c083716290a892a5b7b
Kate Wang <64306646+Katewang0429@users.noreply.github.com>
2021-03-24 18:13:28 +0800

    Add files via upload

---
+++ week4/Week 4.3/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+
+
+@app.route("/")
+def index():
+    if session.get('username') is not None:
+        return redirect('/member')
+    return render_template('signin.html')
+
+
+@app.route("/signin", methods=['POST'])
+def signin():
+    acctName = request.values['acctName']
+    pwd = request.values['pwd']
+
+    if str.upper(acctName) == "TEST" and str.upper(pwd) == "TEST":
+        session['username'] = acctName
+        return redirect('/member')
+    return render_template('error.html')
+
+
+@app.route("/member", methods=['GET'])
+def member():
+    return render_template('member.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    app.run(host='127.0.0.1', port='3000', debug=True)
